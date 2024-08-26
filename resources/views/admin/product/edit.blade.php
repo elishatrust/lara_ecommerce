1c49dc01737c19efa6fae7ec4fbb4bc49809b2b5
bwilukiroelisha <bwilukiroelisha@gmail.com>
2024-08-27 00:58:36 +0300

    Update on Edit product

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -4,43 +4,19 @@
 
 <div class="content-wrapper">
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">{{$title}}</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ url('admin/dashboard')}}">Home</a></li>
-              <li class="breadcrumb-item active">{{$title}}</li>
-            </ol>
-          </div>
-        </div>lass="container-fluid">
-        <div class="row mb-2">
-          <div classlass="container-fluid">
+        <div class="container-fluid">
             <div class="row mb-2">
-              <div class="col-sm-6">
+            <div class="col-sm-6">
                 <h1 class="m-0">{{$title}}</h1>
-              </div>
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="{{ url('admin/dashboard')}}">Home</a></li>
-                  <li class="breadcrumb-item active">{{$title}}</li>
-                </ol>
-              </div>
             </div>
-          </div> ="col-sm-6">
-            <h1 class="m-0">{{$title}}</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ url('admin/dashboard')}}">Home</a></li>
-              <li class="breadcrumb-item active">{{$title}}</li>
-            </ol>
-          </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ url('admin/dashboard')}}">Home</a></li>
+                <li class="breadcrumb-item active">{{$title}}</li>
+                </ol>
+            </div>
+            </div>
         </div>
-      </div> 
-      </div>           
     </div>
 
     <section class="content">
